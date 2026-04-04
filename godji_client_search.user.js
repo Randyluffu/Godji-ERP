6d113bd0529759e01080fed146a9db3a46286dba
Randyluffu <89446030+Randyluffu@users.noreply.github.com>
2026-04-04 19:34:12 +0700

    Update godji_client_search.user.js

--- godji_client_search.user.js
+++ godji_client_search.user.js
@@ -4,8 +4,8 @@
 // @version      5.16
 // @match        https://godji.cloud/*
 // @match        https://*.godji.cloud/*
-// @updateURL    https://raw.githubusercontent.com/Randyluffu/Godji-ERM/main/godji_client_search.user.js
-// @downloadURL  https://raw.githubusercontent.com/Randyluffu/Godji-ERM/main/godji_client_search.user.js
+// @updateURL    https://raw.githubusercontent.com/Randyluffu/Godji-ERP/main/godji_client_search.user.js
+// @downloadURL  https://raw.githubusercontent.com/Randyluffu/Godji-ERP/main/godji_client_search.user.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
